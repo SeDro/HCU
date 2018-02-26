6e381d1ee517440f2d49858a1719ffda4bcb382b
sedro <SeDro@users.noreply.github.com>
2018-02-26 21:21:13 +0100

    added .gitignore and some corrections in user scripts

--- user_scripts/button_sauna.js
+++ user_scripts/button_sauna.js
@@ -1,7 +1,9 @@
 "use strict";
 var actions = require('../app/actions');
+var timing = undefined;
 
 exports.run = (store) => {
+	if(typeof timing !== undefined && timing >= Date.now() - 5000) {
 		var item = store.state.find(item => item.TYPE == "GPIO_output" && item.Name == "Sauna");
 		
 		var tmp =  item.VALUE == 1 ? 0 : 1;
@@ -12,4 +14,8 @@ exports.run = (store) => {
 				var item = store.state.find(item => item.TYPE == "GPIO_output" && item.Name == "Sauna");
 				if(item.VALUE == 1) store.dispatch(actions.updateItem({ID: item.ID, TYPE: item.TYPE, VALUE: 0}));
 			}, 30 * 60 * 1000);
+	}
+	else {
+		timing = Date.now();
+	}
 }
